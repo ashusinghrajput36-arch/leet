60c2349fb560a4fe8725ef598074bb6cbd1ec97d
ashutoshsingh36 <ashusinghrajput36@gmail.com>
2026-06-16 21:57:40 +0530

    Update carpooling.java

--- carpooling.java
+++ carpooling.java
@@ -26,20 +26,19 @@ class Solution {
         Map<Integer, Integer> nextGreaterMap = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         
-        // Traverse through nums2 to find the next greater elements
+
         for (int num : nums2) {
-            // While stack is not empty and the current number is greater than the top of the stack
+            
             while (!stack.isEmpty() && num > stack.peek()) {
                 nextGreaterMap.put(stack.pop(), num);
             }
-            // Push the current number onto the stack
+           
             stack.push(num);
         }
-        
-        // Build the result for nums1 using the map
+
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            // If the element is not in the map, it means there is no greater element, so default to -1
+
             result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
         }
         
